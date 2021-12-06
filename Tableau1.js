e988d89269ff7b4f72c48d33f132b0fb9a1bd04f
Quentin-Allmn <92529322+Quentin-Allmn@users.noreply.github.com>
2021-12-06 13:38:41 +0100

    m

---
+++ Tableau1.js
@@ -0,0 +1,22 @@
+
+class Tableau1 extends Phaser.Scene {
+
+    /**
+     * précharge des assets
+     */
+    preload(){
+
+    }
+
+/**
+ * création de la scène
+ */
+    create()
+{
+
+
+
+    }
+
+
+}
